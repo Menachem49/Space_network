0de7ec5ee0bb5280200f120f079de97ef498776a
aaronco135 <157730636+aaronco135@users.noreply.github.com>
2026-07-13 16:25:55 +0300

    level-2

---
+++ error_magement.py
@@ -0,0 +1,14 @@
+from satellite import *
+import time
+def attempt_transmission(new_mess,space_net):
+    try :
+        space_net.send(new_mess)
+        return
+    except DataCorruptedError as e:
+        print(e)
+        attempt_transmission(new_mess,space_net)
+    except TemporalInterferenceError as e :
+        print(e)
+        time.sleep(2)
+        attempt_transmission(new_mess,space_net)
+            
