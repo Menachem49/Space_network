2fc0c40eacbe6f1f2e2352f0db8d28f06617a266
aaronco135 <157730636+aaronco135@users.noreply.github.com>
2026-07-14 16:36:51 +0300

    level-4

--- main.py
+++ main.py
@@ -2,23 +2,43 @@
 
 from satellite import *
 from error_magement import *
-a = SpaceNetwork(level=1)
+# a = SpaceNetwork(level=1)
 
 
-s1 = Satellite("Sat1",100)
-s2 = Satellite("Sat2",200)
+# s1 = Satellite("Sat1",100)
+# s2 = Satellite("Sat2",200)
 
-new_mess = Packet("hello",s1,s2)
+# new_mess = Packet("hello",s1,s2)
 # a.send(new_mess)
 
-b = SpaceNetwork(level=2)
+# b = SpaceNetwork(level=2)
 
 # attempt_transmission(new_mess,b)
 
 
 
-c = SpaceNetwork(level=3)
+# c = SpaceNetwork(level=3)
+# try :
+#     attempt_transmission(new_mess,c)
+# except BrokenConnectionError:
+#     print("Transmission failed")
+
+
+
+
+d  = SpaceNetwork(level=4)
+earth = Satellite('earth',0,d)
+s1 = Satellite("Sat1",100)
+s2 = Satellite("Sat2",200)
+
+p_final = Packet("hello from earth !!",s1,s2)
+p_earth_to_s1 = RelayPacket(p_final,earth,s1)
+
+
 try :
-    attempt_transmission(new_mess,c)
+   earth.receive_signal(p_earth_to_s1)
 except BrokenConnectionError:
     print("Transmission failed")
+
+
+
--- satellite.py
+++ satellite.py
@@ -1,20 +1,18 @@
 from space_network_lib  import *
-
-
+from relay_packet import *
+from error_magement import attempt_transmission
 
 
 
 
 class Satellite(SpaceEntity):
+ def __init__(self, name, distance_from_earth , space_net = None ):
+      super().__init__(name, distance_from_earth)
+      self.space_net = space_net
     
  def receive_signal(self, packet: Packet):
-        print(self.name ,  "Received:", {packet})
-
-a = SpaceNetwork(level=1)
-
-
-s1 = Satellite("Sat1",100)
-s2 = Satellite("Sat2",200)
-
-new_mess = Packet("hello",s1,s2)
-a.send(new_mess)
+        if isinstance(packet,RelayPacket):
+                print(f'Unwrapping and forwarding to {packet.receiver}')
+                packet = packet.data
+                attempt_transmission(packet,self.space_net)
+        else : print(self.name ,  "Received:", {packet})
